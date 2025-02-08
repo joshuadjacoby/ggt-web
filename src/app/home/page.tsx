d15052952e7224c7187c23e7825260ad95211961
Josh Jacoby <joshuadjacoby@gmail.com>
2025-02-08 10:27:59 -0800

    feat: add home screen images

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -5,10 +5,6 @@ import Image from "next/image";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 
-const solutionsText = `Routines break, meltdowns happen and stress is induced for everyone involved.
-This is where I come in (literally!). Changes in family dynamics are a source of information and insight into the root of a problem or a potential solution.
-I am here to help you embrace change as we uncover resiliency all while dealing with feelings of shame, discomfort and guilt.`;
-
 const quoteText = `“If you put your mind to it, you can accomplish anything.” - Doc Emmet Brown`;
 
 export default function Home() {
@@ -47,16 +43,107 @@ export default function Home() {
           </div>
         </div>
 
-        {/* Introduction Section */}
+        {/* Solutions for Sustainability Section */}
+        <div className="flex flex-col sm:flex-row justify-between items-center bg-white p-4 sm:p-6 rounded-lg shadow-md mt-8 mx-4 sm:mx-0">
+          <div className="w-full sm:w-1/2 p-4">
+            <p className="text-lg sm:text-xl text-gray-700 leading-relaxed">
+              <strong>Virtual and In-Home Services</strong> available throughout
+              California.
+            </p>
+          </div>
+          <div className="w-full sm:w-1/2 p-4">
+            <Image
+              src="/boy_laptop.png"
+              alt="Virtual and In-Home Services"
+              width={400}
+              height={250}
+              objectFit="cover"
+            />
+          </div>
+        </div>
+
+        {/* Routines and Stress Section */}
+        <div className="flex flex-col sm:flex-row-reverse justify-between items-center bg-gray-100 p-4 sm:p-6 rounded-lg shadow-md mt-8 mx-4 sm:mx-0">
+          <div className="w-full sm:w-1/2 p-4">
+            <p className="text-lg sm:text-xl text-gray-700 leading-relaxed">
+              <strong>Routines</strong> break, <strong>meltdowns</strong>{" "}
+              happen, and <strong>stress</strong> is induced for everyone
+              involved.
+            </p>
+          </div>
+          <div className="w-full sm:w-1/2 p-4">
+            <Image
+              src="/screaming.png"
+              alt="Routines and Stress"
+              width={400}
+              height={250}
+              objectFit="cover"
+            />
+          </div>
+        </div>
+
+        {/* Changes in Family Dynamics Section */}
+        <div className="flex flex-col sm:flex-row justify-between items-center bg-white p-4 sm:p-6 rounded-lg shadow-md mt-8 mx-4 sm:mx-0">
+          <div className="w-full sm:w-1/2 p-4">
+            <p className="text-lg sm:text-xl text-gray-700 leading-relaxed">
+              <strong>This is where I come in!</strong> Changes in family
+              dynamics are a source of information and insight into the root of
+              a problem or a potential solution.
+            </p>
+          </div>
+          <div className="w-full sm:w-1/2 p-4">
+            <Image
+              src="/family_table.png"
+              alt="Changes in Family Dynamics"
+              width={400}
+              height={250}
+              objectFit="cover"
+            />
+          </div>
+        </div>
+
+        {/* Embrace Change and Resilience Section */}
+        <div className="flex flex-col sm:flex-row-reverse justify-between items-center bg-gray-100 p-4 sm:p-6 rounded-lg shadow-md mt-8 mx-4 sm:mx-0">
+          <div className="w-full sm:w-1/2 p-4">
+            <p className="text-lg sm:text-xl text-gray-700 leading-relaxed">
+              I am here to help you <strong>embrace change</strong> as we
+              <strong> uncover resiliency</strong> all while dealing with
+              feelings of <strong>shame</strong>, <strong>discomfort</strong>,
+              and <strong>guilt</strong>.
+            </p>
+          </div>
+          <div className="w-full sm:w-1/2 p-4">
+            <Image
+              src="/counseling.png"
+              alt="Embrace Change and Resilience"
+              width={400}
+              height={250}
+              objectFit="cover"
+            />
+          </div>
+        </div>
+
+        {/* Strength-based Approach Section */}
+        <div className="flex flex-col sm:flex-row justify-between items-center bg-white p-4 sm:p-6 rounded-lg shadow-md mt-8 mx-4 sm:mx-0">
+          <div className="w-full sm:w-1/2 p-4">
+            <p className="text-lg sm:text-xl text-gray-700 leading-relaxed">
+              Using a <strong>Strength-based approach</strong>, your goals and
+              hopes for the future are much closer than you think!
+            </p>
+          </div>
+          <div className="w-full sm:w-1/2 p-4">
+            <Image
+              src="/strength.png"
+              alt="Strength-based Approach"
+              width={400}
+              height={250}
+              objectFit="cover"
+            />
+          </div>
+        </div>
+
+        {/* Quote Section */}
         <div className="bg-white p-6 rounded-lg shadow-md mt-8 mx-4 sm:mx-0">
-          <h2 className="text-2xl sm:text-3xl font-semibold text-gray-800 mb-4">
-            Solutions for Sustainability
-          </h2>
-          <p className="text-lg sm:text-xl text-gray-700 leading-relaxed whitespace-pre-line">
-            {solutionsText}
-          </p>
-          <br />
-          {/* Quote Section */}
           <blockquote className="text-lg sm:text-2xl text-gray-900 italic font-semibold border-l-4 border-red-600 pl-4">
             {quoteText}
           </blockquote>
